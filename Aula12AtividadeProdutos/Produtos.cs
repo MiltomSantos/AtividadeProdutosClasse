14453bac4e10b50ecbade4f15e94d62383c15ba5
MiltomSantos <122657746+MiltomSantos@users.noreply.github.com>
2023-07-25 17:20:49 -0300

    Add files via upload

---
+++ Aula12AtividadeProdutos/Produtos.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula12AtividadeProdutos
+{
+    internal class Produtos
+    {
+        public string nome;
+        public string descricao;
+        public int quantidade;
+        public double preco;
+        public bool status;
+
+        public void mostra()
+        {
+            Console.WriteLine($"Produto: {nome}, Quantidade: {quantidade}, Preço: {preco}, Status: {status}\nDescrição: {descricao}");
+        }
+
+        public void desativa()
+        {
+            Console.WriteLine("Quer desativar o produto?\n'S' para sim\n'N' para não");
+            string resp = Console.ReadLine();
+            if (resp=="S")
+            {
+                status= false;
+            }
+            else
+            {
+                status= true;
+            }
+        }
+
+        public void quantidadeDesativada()
+        {
+            Console.WriteLine("Quer zerar a quantidade?\n");
+            string resp = Console.ReadLine();
+            if (resp == "S")
+            {
+                quantidade=0;
+            }
+            
+        }
+    }
+}
---
+++ Aula12AtividadeProdutos/Program.cs
@@ -0,0 +1,42 @@
+﻿using Aula12AtividadeProdutos;
+
+Console.WriteLine("");
+
+Produtos darkSouls1 = new Produtos();
+darkSouls1.nome = "Dark Souls 1";
+darkSouls1.descricao = "Um jogo dificil pra caramba";
+darkSouls1.quantidade = 4;
+darkSouls1.preco = 119.50;
+darkSouls1.status = true;
+
+darkSouls1.mostra();
+
+darkSouls1.desativa();
+darkSouls1.quantidadeDesativada();
+
+
+Produtos darkSouls2 = new Produtos();
+darkSouls2.nome = "Dark Souls 2";
+darkSouls2.descricao = "Um jogo ruim pra caramba";
+darkSouls2.quantidade = 40;
+darkSouls2.preco = 50;
+darkSouls2.status = true;
+
+darkSouls2.mostra();
+darkSouls2.desativa();
+darkSouls2.quantidadeDesativada();
+
+Produtos darkSouls3 = new Produtos();
+darkSouls3.nome = "Dark Souls 3";
+darkSouls3.descricao = "Um jogo dificil pra caramba!!!!!!";
+darkSouls3.quantidade = 2;
+darkSouls3.preco = 127.50;
+darkSouls3.status = true;
+
+darkSouls3.mostra();
+darkSouls3.desativa();
+darkSouls3.quantidadeDesativada();
+
+darkSouls1.mostra();
+darkSouls2.mostra();
+darkSouls3.mostra();
